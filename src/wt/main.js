f3cd37653b91456a39e5e9d3bb993275918db623
Vladyslav Bilousov <vladyslav_bilousov@epam.com>
2022-06-05 11:58:10 +0300

    feat: task1/ updated new wt resolve

--- src/wt/main.js
+++ src/wt/main.js
@@ -6,17 +6,31 @@ import {fileURLToPath} from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const coreNum = cpus().length;
+const workerPromisesArr = [];
 
 export const performCalculations = async () => {
     const path = resolve(`${__dirname}/worker.js`);
     if(isMainThread) {
         for (let i = 10; i < coreNum + 10; i++) {
             const worker = new Worker(path, { workerData: { value: i } });
-            worker.on('message', (result) => {
-                console.log(`Worker ${worker.threadId} result on n = ${i}: ${result}`);
-            });
+            const promise = new Promise((resolve, reject) => {
+                worker.on('message', resolve);
+                worker.on('error', reject);
+            })
+
+            workerPromisesArr.push(promise);
         }
+
+        let fiboResults = await Promise.allSettled(workerPromisesArr);
+        return fiboResults.map(result => {
+            if (result.status === 'fulfilled') {
+                return { status: 'resolved', data: result.value }
+            } else {
+                return { status: 'error', data: null }
+            }
+        });
     }
 };
 
-performCalculations();
+const res = await performCalculations();
+console.log(res);
